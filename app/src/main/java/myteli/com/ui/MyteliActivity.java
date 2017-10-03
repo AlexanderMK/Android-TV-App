00ccd228e7a7bb8ca955e7d826bd44888485cc88
Alexander <mburukungu@gmail.com>
2017-10-03 19:57:46 +0300

    created myteli activity for search requests

--- app/src/main/java/myteli/com/ui/MyteliActivity.java
+++ app/src/main/java/myteli/com/ui/MyteliActivity.java
@@ -6,7 +6,7 @@ import android.support.v4.app.FragmentActivity;
 /**
  * This parent class contains common methods that run in every activity such as search.
  */
-public abstract class LeanbackActivity extends FragmentActivity {
+public abstract class MyteliActivity extends FragmentActivity {
     @Override
     public boolean onSearchRequested() {
         startActivity(new Intent(this, SearchActivity.class));

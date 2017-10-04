9b47b15822e6f5356f1bc2544f1a409b0003097e
Alexander <mburukungu@gmail.com>
2017-10-04 12:09:10 +0300

    this class instances the welcome message in mobile

---
+++ app/src/main/java/myteli/com/mobile/MobileWelcomeActivity.java
@@ -0,0 +1,15 @@
+package myteli.com.mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import myteli.com.R;
+
+public class MobileWelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mobile_welcome);
+    }
+}
